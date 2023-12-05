17877118597cea6425d936a3b572fb765abe2b2f
Davenn69 <142876715+Davenn69@users.noreply.github.com>
2023-12-05 10:58:20 +0700

    Add files via upload

---
+++ src/Component/kursiNotAvailable.jsx
@@ -0,0 +1,9 @@
+import '../SeatPage.css';
+import { useState } from 'react';
+const KursiNotAvailable = (props)=>{
+    
+    return (
+        <div className={`display-one-seat-not-available`}>X</div>
+    )
+}
+export default KursiNotAvailable;
