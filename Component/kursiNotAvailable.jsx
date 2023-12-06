d5269857c41a513de79c0889eb6861d10a30dcc3
Shayp77 <148016597+Shayp77@users.noreply.github.com>
2023-12-06 19:44:34 +0700

    Delete Component directory

--- Component/kursiNotAvailable.jsx
+++
@@ -1,9 +0,0 @@
-import '../SeatPage.css';
-import { useState } from 'react';
-const KursiNotAvailable = (props)=>{
-    
-    return (
-        <div className={`display-one-seat-not-available`}>X</div>
-    )
-}
-export default KursiNotAvailable;
